d75e17aa66ab99cfa7b6414d689c9b8c1a93977e
Nadav Shaar <nadavshaar@gmail.com>
2020-06-22 22:26:25 +0300

    First commit bitches!

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-const path = require('path');
-const port = 3000;
-
-app.use(express.static(path.resolve(__dirname, '..')))
-
-app.listen(port, () => console.log(`Landing Page listening at http://localhost:${port}`))
