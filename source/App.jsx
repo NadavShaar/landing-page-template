eff401c1eb8b1b3cdb3d8b06f7fadd3e9261dcdb
ilayofir <ilayofir@gmail.com>
2020-06-22 14:42:48 +0300

    a

--- source/App.jsx
+++ source/App.jsx
@@ -9,7 +9,7 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div style={{height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+            <div style={{height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', overflow: 'auto'}}>
                 <p style={{fontSize: 20, fontWeight: 500}}>Our Team</p>
                 <p>We are a team of veteran fullstack web developers, specialized in react apps, dedicated to building awesome applications from A to Z.</p>
                 <p>An accumulative experience of over 30 years with various frameworks and libraries, and a full understanding of the needs of modern web applications, gives us the tools to design, advise, build, test, deploy and maintain a working web application.</p>
--- source/Card.jsx
+++ source/Card.jsx
@@ -9,12 +9,15 @@ export default class Card extends React.Component {
         const { member } = this.props;
 
         return (
-            <div style={{ height: 300, display: 'flex', flexDirection: 'column' }}>
-                <div style={{ height: 60, display: 'flex' }}>
-                    <img style={{ height: 60, width: 60}} src='/resources/avatar.png'></img>
-                    <p>{member.name}</p>
+            <div style={{ height: 300, display: 'flex', flexDirection: 'column', border: 'solid 1px #ddd' }}>
+                <div style={{ height: 100, display: 'flex' }}>
+                    <img style={{ height: 100, width: 100}} src='/resources/avatar.png'></img>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <p>{member.name}</p>
+                        <p>{member.description}</p>
+                    </div>
                 </div>
-                <p>{member.description}</p>
+                <p>{member.summary}</p>
             </div>
         );
     }
